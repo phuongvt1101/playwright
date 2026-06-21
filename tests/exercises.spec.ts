7d13d04ac2eb50aebaaf6181e2e96ab27d9906c5
phuong1101 <phuongmumi1994@gmail.com>
2026-06-21 17:16:50 +0700

    test commit

--- tests/exercises.spec.ts
+++ tests/exercises.spec.ts
@@ -14,12 +14,12 @@ test ('checkbox', async ({page}) => {
 test ( 'dropdown', async ({page}) => {
  await page.goto('https://the-internet.herokuapp.com/dropdown');
 
-//  await page.locator('#dropdown').selectOption('Option 1');
-//  console.log('value log', await page.locator('option:checked').textContent());
-//  await expect(page.locator('option:checked')).toHaveText('Option 1');
+ await page.locator('#dropdown').selectOption('Option 1');
+ console.log('value log', await page.locator('option:checked').textContent());
+ await expect(page.locator('option:checked')).toHaveText('Option 1');
 
-await page.getByRole('combobox').selectOption('Option 1');
-await expect(page.locator('option:checked')).toHaveText('Option 1');
+// await page.getByRole('combobox').selectOption('Option 1');
+// await expect(page.locator('option:checked')).toHaveText('Option 1');
 
 });
 
